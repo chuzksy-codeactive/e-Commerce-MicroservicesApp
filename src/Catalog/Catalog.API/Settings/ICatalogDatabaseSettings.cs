674c081ca07257063051caf5e18a0f41970ed9a7
Onuchukwu Chika Augustine <chuzksy@yahoo.com>
2020-07-26 03:20:06 +0100

    - Add Configuration settings for CatalogDatabaseSettings - Add the project dependencies - Add MongoDB Driver and connection string

---
+++ src/Catalog/Catalog.API/Settings/ICatalogDatabaseSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Catalog.API.Settings
+{
+    public interface ICatalogDatabaseSettings
+    {
+        string CollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+}
--- src/Catalog/Catalog.API/Startup.cs
+++ src/Catalog/Catalog.API/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Catalog.API.Data.Interfaces;
+using Catalog.API.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -9,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace Catalog.API
 {
@@ -25,6 +28,17 @@ namespace Catalog.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            #region Configuration Dependencies
+            services.Configure<CatalogDatabaseSettings>(Configuration.GetSection(nameof(CatalogDatabaseSettings)));
+
+            services.AddSingleton<ICatalogDatabaseSettings>(sp =>
+                sp.GetRequiredService<IOptions<CatalogDatabaseSettings>>().Value);
+            #endregion
+
+            #region Project Dependencies
+            services.AddTransient<ICatalogContext, ICatalogContext>(); 
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
