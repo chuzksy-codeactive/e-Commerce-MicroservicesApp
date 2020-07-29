88a345dec5970c2bc8fbce2d0e985692b3abeb00
chuzksy-codeactive <chuzksy@gmail.com>
2020-07-29 04:33:53 +0100

    Create Order DbContext and add seeding functionality into the DB

---
+++ src/Ordering/Ordering.Infrastructure/Data/OrderContextSeed.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Ordering.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Data
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext, ILoggerFactory loggerFactory, int? retry = 0)
+        {
+            int retryForAvailability = retry.Value;
+
+            try
+            {
+                // INFO: Run this if using a real database. Used to automaticly migrate docker image of sql server db.
+                orderContext.Database.Migrate();
+                //orderContext.Database.EnsureCreated();
+
+                if (!orderContext.Orders.Any())
+                {
+                    orderContext.Orders.AddRange(GetPreconfiguredOrders());
+                    await orderContext.SaveChangesAsync();
+                }
+            }
+            catch (Exception exception)
+            {
+                if (retryForAvailability < 5)
+                {
+                    retryForAvailability++;
+                    var log = loggerFactory.CreateLogger<OrderContextSeed>();
+                    log.LogError(exception.Message);
+                    await SeedAsync(orderContext, loggerFactory, retryForAvailability);
+                }
+                throw;
+            }
+        }
+
+        private static IEnumerable<Order> GetPreconfiguredOrders()
+        {
+            return new List<Order>()
+            {
+                new Order() { UserName = "chuzksy", FirstName = "Onuchukwu", LastName = "Chika", EmailAddress = "chuzksy@gmail.com", AddressLine = "Shomolu", TotalPrice = 5239 },
+                new Order() { UserName = "sam", FirstName = "Samuel", LastName = "Olabiyi", EmailAddress ="olabiyi.sam@gmail.cm", AddressLine = "Omonile", TotalPrice = 3486 }
+            };
+        }
+    }
+}
