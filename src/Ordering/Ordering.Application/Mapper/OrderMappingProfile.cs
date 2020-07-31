51b5feff264fc315cf96af14af17650090622c2f
chuzksy-codeactive <chuzksy@yahoo.com>
2020-07-31 05:26:01 +0100

    - Implement CQRS with Mediator design pattern - Add Query command for Order entity - Add AutoMapper

---
+++ src/Ordering/Ordering.Application/Mapper/OrderMappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Ordering.Application.Responses;
+using Ordering.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ordering.Application.Mapper
+{
+    public class OrderMappingProfile : Profile
+    {
+        public OrderMappingProfile()
+        {
+            CreateMap<Order, OrderResponse>().ReverseMap();
+        }
+    }
+}
